7363b413605c1a445719184061febe79f6f26f8a
TalhaAkkurt <115419923+TalhaAkkurt@users.noreply.github.com>
2022-10-23 19:31:43 +0300

    Create odev1.py

---
+++ odev1.py
@@ -0,0 +1,24 @@
+a = "Ankara"
+b = "İstanbul"
+print(a!="İstanbul")
+print(a=="İstanbul")
+
+
+print("---------------")
+
+
+
+c = "Sn Ahmet bey"
+d = " öğrencinizin toplam devamsızlığı 10 gündür"
+print(c+d)
+
+
+
+
+print("---------------")
+
+aboneNumarası = 123456;
+tuketimDonemi = 30;
+tuketimTutari = tuketimDonemi * 1;
+
+print("Sayın ", aboneNumarası, "nolu abonemiz", tuketimDonemi, "dönemi faturanız", tuketimTutari, "TL'dir. Python Belediyesi")
